cff66bd1ff90b765325040e60a743bd289827b98
AdryDev <adrydevmateo@gmail.com>
2024-03-22 23:38:16 -0400

    added golang support

--- lua/plugins/package_managers.lua
+++ lua/plugins/package_managers.lua
@@ -1,32 +1,36 @@
 local masonOpts = {
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
 }
 
 local masonLspOpts = {
-	ensure_installed = { "lua_ls" },
+  ensure_installed = { "lua_ls", "gopls" },
 }
 
 return {
-	{ "williamboman/mason-lspconfig.nvim" },
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup(masonOpts)
-			require("mason-lspconfig").setup(masonLspOpts)
-			local lsp = require("lspconfig")
+  { "williamboman/mason-lspconfig.nvim" },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup(masonOpts)
+      require("mason-lspconfig").setup(masonLspOpts)
+      local lsp = require("lspconfig")
       local cap = require('cmp_nvim_lsp').default_capabilities()
 
-			-->> [ Lsp Configurations
-			lsp.lua_ls.setup({
+      -->> [ Lsp Configurations
+      lsp.lua_ls.setup({
         capabilities = cap
       })
-			--<< Lsp COnfigurations ]
-		end,
-	},
+      lsp.gopls.setup {
+        capabilities = cap
+      }
+
+      --<< Lsp COnfigurations ]
+    end,
+  },
 }
